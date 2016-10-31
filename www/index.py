6fa8376bd6884c916a813eba65564b59c7968510
YuPeng <13463121352@163.com>
2016-10-31 08:38:35 +0800

    index.py

---
+++ www/index.py
@@ -0,0 +1,14 @@
+import os,sys
+
+
+def main():
+	print("Content-type:text/html\n\n")
+	print("<html>")
+	print("<body>")
+	print("<h1>CGI Test</h1>")
+	print("</body>")
+	print("</html>")
+
+main()
+
+
